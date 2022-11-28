048c3e21215595f9f2f01cc33c74496ac6a48e85
Jumpy Cat <77382504+catsmeow4753@users.noreply.github.com>
2022-11-28 13:36:51 +0100

    Update Broadcast.py

--- Broadcast.py
+++ Broadcast.py
@@ -3,13 +3,13 @@ import time
 from socket import *
 from datetime import datetime
 
-s = socket(AF_INET, SOCK_DGRAM)
+sense = socket(AF_INET, SOCK_DGRAM)
 #s.bind(('', 14593))     # (ip, port)
 # no explicit bind: will bind to default IP + random port
-s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+sense.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 while True:
 	for event in sense.stick.get_events():
 		data = event.direction, event.action
-		s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
+		sense.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
 		print(data)
 		time.sleep(1)
