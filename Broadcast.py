d5752485fc9b3a9e9afe9ded2196049bc261cf97
Jumpy Cat <77382504+catsmeow4753@users.noreply.github.com>
2022-11-28 13:53:21 +0100

    Update Broadcast.py

--- Broadcast.py
+++ Broadcast.py
@@ -10,6 +10,6 @@ s = socket(AF_INET, SOCK_DGRAM)
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 while True:
 	for event in sense.stick.get_events():
-		data = str(event.direction, event.action)
+		data = "Stick direction: " + str(event.direction)
 		s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
 		print(data)
