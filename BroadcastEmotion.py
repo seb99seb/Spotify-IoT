92873e7ad316894919c418206ecac09750eb0088
Jumpy Cat <77382504+catsmeow4753@users.noreply.github.com>
2022-11-28 14:02:36 +0100

    Create BroadcastEmotion.py

---
+++ BroadcastEmotion.py
@@ -0,0 +1,15 @@
+BROADCAST_TO_PORT = 7000
+import time
+from socket import *
+from datetime import datetime
+from sense_hat import SenseHat
+sense = SenseHat()
+s = socket(AF_INET, SOCK_DGRAM)
+#s.bind(('', 14593))     # (ip, port)
+# no explicit bind: will bind to default IP + random port
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+while True:
+	for event in sense.stick.get_events():
+		data = "Direction: " + str(event.direction) + ", Action: " + str(event.action)
+		s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
+		print(data)
