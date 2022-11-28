ac742070e40c23d0a5b95ff25f1f89c8d33e492c
Jumpy Cat <77382504+catsmeow4753@users.noreply.github.com>
2022-11-28 13:50:34 +0100

    Update Broadcast.py

--- Broadcast.py
+++ Broadcast.py
@@ -10,7 +10,6 @@ s = socket(AF_INET, SOCK_DGRAM)
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 while True:
 	for event in sense.stick.get_events():
-		data = "Hi"
+		data = str(event.direction, event.action)
 		s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
 		print(data)
-		time.sleep(1)
