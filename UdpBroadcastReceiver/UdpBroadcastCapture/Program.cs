bf3d632c610d8946b3809c68af54ddd987274c65
Jumpy Cat <77382504+catsmeow4753@users.noreply.github.com>
2022-11-30 09:50:16 +0100

    Broadcast Program

---
+++ UdpBroadcastReceiver/UdpBroadcastCapture/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpBroadcastCapture
+{
+    class Program
+    {
+        // https://msdn.microsoft.com/en-us/library/tst0kwb1(v=vs.110).aspx
+        // IMPORTANT Windows firewall must be open on UDP port 7000
+        // https://www.windowscentral.com/how-open-port-windows-firewall
+        // Use the network MGV-xxx to capture from local IoT devices (fake or real)
+        private const int Port = 7000;
+        //private static readonly IPAddress IpAddress = IPAddress.Parse("192.168.5.137"); 
+        // Listen for activity on all network interfaces
+        // https://msdn.microsoft.com/en-us/library/system.net.ipaddress.ipv6any.aspx
+        static void Main()
+        {
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, Port);
+            using (UdpClient socket = new UdpClient(ipEndPoint))
+            {
+                IPEndPoint remoteEndPoint = new IPEndPoint(0, 0);
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast {0}", socket.Client.LocalEndPoint);
+                    byte[] datagramReceived = socket.Receive(ref remoteEndPoint);
+
+                    string message = Encoding.ASCII.GetString(datagramReceived, 0, datagramReceived.Length);
+                    Console.WriteLine("Receives {0} bytes from {1} port {2} message {3}", datagramReceived.Length, remoteEndPoint.Address, remoteEndPoint.Port, message);
+
+                    RecieveStickEvent(message);
+                }
+            }
+        }
+
+        public static void RecieveStickEvent(string message)
+        {
+            string[] result = message.Split(' ');
+
+            if (result.Length != 2)
+            {
+                throw new Exception($"Message must be 2 strings. Message length: {result.Length}");
+            }
+
+            if (result[1] == "pressed")
+            {
+                string direction = result[0];
+
+                switch (direction)
+                {
+                    case "up":
+                        Console.WriteLine($"Direction: {direction}, emotion is neutral.");
+                        break;
+                    case "down":
+                        Console.WriteLine($"Direction: {direction}, emotion is cleared.");
+                        break;
+                    case "left":
+                        Console.WriteLine($"Direction: {direction}, emotion is happy.");
+                        break;
+                    case "right":
+                        Console.WriteLine($"Direction: {direction}, emotion is sad.");
+                        break;
+                    case "middle":
+                        Console.WriteLine($"Direction: {direction}, emotion is cleared.");
+                        break;
+                    default:
+                        Console.WriteLine($"Message is not a valid direction.");
+                        break;
+                }
+            }
+        }
+    }
+}
