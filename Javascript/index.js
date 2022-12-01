12876c9c6c468fc038ff67dc7ed34bc4360e555f
seb99seb <sebvangpedersen@hotmail.dk>
2022-12-01 10:38:10 +0100

    Token working and device id gotten

--- Javascript/index.js
+++ Javascript/index.js
@@ -3,58 +3,38 @@ Vue.createApp({
         return {
             clientId: '849bcded0aa04ffa855b5bd3381c7284',
             clientSecret: '25bed5e8f08a43ac9f914b920dae2b4b',
-            token: "",
-            scopes: "user-read-playback-state",
+            scopes: "user-read-playback-state playlist-read-private user-modify-playback-state",
             redirectURI: "http://localhost:5501/",
             startupDone: false,
-            getCategoriesDone: false,
-            genres: [],
             deviceId: "",
-            deviceIdGotten: false,
-            goodtoken: ""
+            auth: "",
+            token: "",
+            xhr: ""
         }
     },
     methods: {
-        async getToken(){
-            const result = await fetch('https://accounts.spotify.com/api/token', {
-                method: 'POST',
-                headers: {
-                    'Content-Type' : 'application/x-www-form-urlencoded', 
-                    'Authorization' : 'Basic ' + btoa(this.clientId + ':' + this.clientSecret)
-                },
-                body: 'grant_type=client_credentials'
-            });
-            const data = await result.json()
+        fetchAccessToken(){
+            let body = 'grant_type=authorization_code'
+            body += '&code=' + this.auth
+            body += '&redirect_uri=' + this.redirectURI
+            body += '&client_id=' + this.clientId
+            body += '&client_secret=' + this.clientSecret
+            this.callAuthorizationApi(body)
+        },
+        callAuthorizationApi(body){
+            this.xhr = new XMLHttpRequest()
+            this.xhr.open('POST', 'https://accounts.spotify.com/api/token', true)
+            this.xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+            this.xhr.setRequestHeader('Authorization', 'Basic ' + btoa(this.clientId + ':' + this.clientSecret))
+            this.xhr.send(body)
+            console.log(this.xhr)
+            this.xhr.onload = this.handleAuthorizationResponse
+            console.log(this.xhr.responseText)
+        },
+        handleAuthorizationResponse(){
+            var data = JSON.parse(this.xhr.responseText)
             this.token = data.access_token
-            this.startupDone = true
-        },
-        /*getTokenYEP(){
-            return 'https://accounts.spotify.com/api/token', {
-                method: 'POST',
-                headers: {
-                    'client_id' : encodeURIComponent(this.clientId),
-                    '&client_secret' : encodeURIComponent(this.clientSecret),
-                    '&grant_type' : 'authorization_code',
-                    '&code' : encodeURIComponent(this.goodtoken),
-                    '&redirect_uri' : encodeURIComponent(this.redirectURI)
-                },
-                body: 'grant_type=client_credentials'
-            }
-        },*/
-        getPermToken(){
-            return 'https://accounts.spotify.com/authorize?client_id=' + this.clientId
-            + '&redirect_uri=' + encodeURIComponent(this.redirectURI)
-            + '&scope=' + encodeURIComponent(this.scopes)
-            + '&response_type=code';
-        },
-        async getGenres(){
-            const result = await fetch(`https://api.spotify.com/v1/browse/categories`, {
-                method: 'GET',
-                headers: { 'Authorization' : 'Bearer ' + this.token}
-            });
-            const data = await result.json()
-            this.genres = data.categories.items
-            this.getCategoriesDone = true
+            console.log(this.token)
         },
         async getDeviceId(){
             const result = await fetch(`https://api.spotify.com/v1/me/player/devices`, {
@@ -62,12 +42,12 @@ Vue.createApp({
                 headers: { 'Authorization' : 'Bearer ' + this.token}
             })
             const data = await result.json()
-            this.deviceId = data.devices.id
-            this.deviceIdGotten = true
+            this.deviceId = data.devices[0].id
+            console.log(this.deviceId)
         },
         getFragmentIdentifier(){
             const urlParams = new URLSearchParams(window.location.search);
-            this.goodtoken = urlParams.get('code');
+            this.auth = urlParams.get('code');
         }
     }
 }).mount("#app")
