bbfe0c6cbec2f6f2780eef86456d026157bf8754
Jumpy Cat <77382504+catsmeow4753@users.noreply.github.com>
2022-12-01 14:12:04 +0100

    UDP and REST

---
+++ UdpBroadcastReceiver/SpotifyREST/Controllers/DirectionController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpotifyREST.Managers;
+using SpotifyREST.Models;
+
+namespace SpotifyREST.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DirectionController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Get(string message)
+        {
+            DirectionManager.Create(message);
+
+            return StatusCode(201);
+        }
+
+        [HttpGet]
+        [Route("id")]
+        public IActionResult Get(int id)
+        {
+            return Ok(DirectionManager.Get(id));
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Direction> directions = DirectionManager.GetAll();
+            
+            if (directions.Count > 0)
+            {
+                return Ok(directions);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+    }
+}
