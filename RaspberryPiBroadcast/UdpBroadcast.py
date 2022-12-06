ea69014e569a4e2141ab8c71f5446b181e95b1d8
Jumpy Cat <77382504+catsmeow4753@users.noreply.github.com>
2022-12-06 12:17:28 +0100

    UdpBroadcast Update

--- RaspberryPiBroadcast/UdpBroadcast.py
+++ RaspberryPiBroadcast/UdpBroadcast.py
@@ -1,16 +1,22 @@
 from socket import *
 import requests
+import urllib3
 
-URI = "https://localhost:7234/swagger/index.html"
+http = urllib3.PoolManager()
+urllib3.disable_warnings()
+
+URI = "https://localhost:7234/Direction"
 
 serverPort = 7234
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-
 serverSocket.bind(('', serverPort))
-response = requests.get(URI)
 
 print("The server is ready to receive")
-
 while True:
+    #Receive message from RaspberryPi
     message, clientAddress = serverSocket.recvfrom(2048)
-    print(message.decode())
+    print("Message Received: " + message.decode())
+    #Post message to REST Service
+    url = URI + "?message=" + message.decode()
+    response = requests.post(url=url, verify=False)
+    print("Status Code: " + str(response.status_code))
