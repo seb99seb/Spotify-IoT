362f7acd2b234a74c7538e5ffb28ac6b35dad38b
Freja Gudmandsen <ferling.gud@gmail.com>
2022-12-13 11:01:19 +0100

    comments

--- SensifyREST/SensifyREST/Managers/MoodsManager.cs
+++ SensifyREST/SensifyREST/Managers/MoodsManager.cs
@@ -4,7 +4,10 @@ namespace SensifyREST.Managers
 {
     public class MoodsManager
     {
+        /// Vi bruger vores MoodService til at styre 'objekterne' i denne klasse
         private static MoodsService _moodsService = new MoodsService();
+
+        /// Metode til at hente spillelisteID på baggrund af humøret. 
         public string GetPlaylistIdByMood(string mood)
         {
             _moodsService.CheckMood(mood);
