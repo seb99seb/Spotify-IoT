f442ef5c35a1ad918314cc5849fb142c6ef616e5
Freja Gudmandsen <ferling.gud@gmail.com>
2022-12-13 11:03:19 +0100

    comments manager

--- SensifyREST/SensifyREST/Managers/MoodsManager.cs
+++ SensifyREST/SensifyREST/Managers/MoodsManager.cs
@@ -13,15 +13,22 @@ namespace SensifyREST.Managers
             _moodsService.CheckMood(mood);
             return _moodsService.GetPlaylistId(mood);
         }
+
+        /// void - en metode der ikke returnerer noget
+        /// Her bruger vi den til at gemme spillelisteID'et sammen med et humør
         public void SavePlaylistIdToMood(string mood, string playlistId)
         {
             _moodsService.CheckMood(mood);
             _moodsService.SavePlaylistId(mood, playlistId);
         }
+
+        /// Denne metode opdaterer humøret, på baggrund af retningen af joystikket. 
         public void UpdateCurrentMood(string direction)
         {
             _moodsService.UpdateCurrentMood(direction);
         }
+
+        /// Henter det sidst registrerede humør, og sætter det til det nuværende humør. 
         public string GetCurrentMood()
         {
             return _moodsService.GetCurrentMood();
